a1b7694dabc4b7ba7f0975685a0cbf7e84a1b10e
amanjpro <amanj.sherwany@usi.ch>
2017-12-09 01:13:00 -0500

    Add a way to provide extra args to shellcheck

--- src/main/scala/ShellCheckPlugin.scala
+++ src/main/scala/ShellCheckPlugin.scala
@@ -9,14 +9,17 @@ object ShellCheckPlugin extends AutoPlugin {
 
   object autoImport {
     lazy val shellCheck = inputKey[Unit]("Execute shellcheck")
+    lazy val shellCheckArgs = settingKey[Seq[String]]("Extra arguments to shellcheck")
   }
 
   import autoImport._
 
   override lazy val buildSettings = Seq(
+    shellCheckArgs := Seq.empty,
     shellCheck := {
+      val extraArgs = shellCheckArgs.value.toList
       val args = DefaultParsers.spaceDelimited("<arg>").parsed.toList
-      if(("shellcheck" :: args).! != 0)
+      if(("shellcheck" :: (extraArgs ++ args)).! != 0)
         throw new IllegalStateException("shellcheck failed")
     }
   )
